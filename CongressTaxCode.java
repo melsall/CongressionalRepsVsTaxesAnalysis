5b008e30d55f9ddb8af72968ce49f24495436350
melsall <93349505+melsall@users.noreply.github.com>
2022-11-01 20:22:46 -0400

    Added extremely detailed pseudocode for new highly parameterized version of file. This file now has the outline of a main method, a method for comparing the average of a certain type of tax across republican/democratic years, and a helper method for splitting the house dataset into easier to digest array lists of democrat and republican years

--- CongressTaxCode.java
+++ CongressTaxCode.java
@@ -5,92 +5,50 @@ public class CongressTaxCode {
     public static void main (String [] args)throws FileNotFoundException{
         File houseStatistics = new File("House_Dataset.csv");
         File taxStatistics = new File("Tax_Dataset.csv");
-        compareIncomeTaxesBasedOnParty(houseStatistics, taxStatistics);
-
     }
     /* Question 1: 
     The democratic party is generally the party associated with higher income taxes. 
-    Is there actually a positive correlation between the percentage of democrats in power in the house and 
-    an increase in individual income taxes from 1946-2018? */
+    Is the average percentage of federal revenue from income tax actually higher when democrats have a house majority?  */
+    /* Question 2: During Republican dominated years, is the average percentage of revenue from corporate income taxes a
+    ctually lower than in democratic years? */
 
-    // pseudo code question 1
-    /* 
-    1. Read in Congressional dataset
-    2. Identify each year as democrat/republican based on percentage of seats won by democrats
-    3. make corresponding arraylist in tax dataset that groups 2 years together and finds averages
+    /* Used the houseStatistics and the taxStatistics arrays to compare the percent of federal revenue from a certain type of taxes within a set number of years */
+    public static void compareTaxesBasedOnParty(File houseStatistics, File taxStatistics, int minYear, int maxYear, String typeOfTax) throws FileNotFoundException{
+      // Using the splitIntoRepubAndDemYears method, save two arraylists as repubYears and demYears
+      // Initialize some variables that will be used as running totals: demTotalPercentages, repubTotalPercentages, currentPartyInPower
+      // Open a Scanner to access taxStatistics file
+      // Save the header of the taxStatistics file (save the index of "Year" column to a variable, save the index of  typeOfTax to a variable)
+      // Use a scanner to iterate through lines of taxStatistics, stop when the tax dataset runs out of years
+      // Use a line scanner to isolate each line
+      // For each line, save the element in the year column to a variable currentYear
+      // check if currentYear appears in the democratic years or republican years arraylist (if it appears in neither, it is not an election year, so it should be added to the previous total that was incremented)
+      // change currentPartyInPower in accordance with if the year is a democrat or republican year, also check if it contains "estimate" in it, if it does do not count it
+      // depending on the arraylist that the currentYear appears in, add the value of the typeOfTax column to the total for that political party
+      // Access taxStatistics, get each year
+      /* Calculate two variables: averageRevenueFromTaxInDemYears would be calcluating by doing demTotalPercentages/demYears.size();, the republican equivalent
+      would be calculated in the same manner */
+      // Print out a statement specifiying the type of tax, the percent from democratic party and the percent from the republican party
+    }
 
-    */
-    public static void compareIncomeTaxesBasedOnParty(File houseStatistics, File taxStatistics) throws FileNotFoundException{
-        // Create Scanners 
-        Scanner houseScanner = new Scanner(houseStatistics);
-        Scanner taxScanner = new Scanner(taxStatistics);
-        // Get the header of the congressional dataset
-        houseScanner.nextLine(); // this is necessary because there are two rows of headers in this particular dataset
-        houseScanner.nextLine();
-        houseScanner.nextLine();
-        String [] houseHeader = houseScanner.nextLine().split(",");
-        ArrayList <String> houseHeaderAL =  new ArrayList <> (Arrays.asList(houseHeader));
-        int houseYearIndex = houseHeaderAL.indexOf("Year"); // also need to be able to access the year
-        int demPercentSeatsWonIndex = houseHeaderAL.indexOf("Percentage of seats won"); // get index of democratic percentage of seats won column
-        // Get header of tax dataset
-        String [] taxHeader = taxScanner.nextLine().split(",");
-        ArrayList <String> taxHeaderAL = new ArrayList <> (Arrays.asList(taxHeader));
-        // Also need to know the index of the individual income tax bucket
-        int incomeTaxIndex = taxHeaderAL.indexOf("Individual Income Taxes");
-        // Need to move taxScanner to start at 1946 since the two have different start dates
-        boolean keepIterating = true;
-        int whatLine = 0;
-        while(keepIterating){
-            whatLine++;
-            System.out.println("Line number " + whatLine);
-            String [] taxLine = taxScanner.nextLine().split(",");
-            ArrayList <String> taxLineAL = new ArrayList <> (Arrays.asList(taxLine));
-            if (taxLineAL.indexOf("1946") != -1){
-                System.out.println("BREAK");
-                break;
-            }
-        }
-        /* the years that be considered in this project are the years 1946-2012 although the house dataset contains data up to 2018. The tax dataset 
-        has "estimates" of taxes up to 2018, but only has real data up to 2021. Since the tax dataset doesn't have real data up to that 2018, the boolean endYear must be created to ensure the 
-        Scanner doesn't go past 2012 in the datsets and therefore treat estimated data as real data */
-        int endYear = 2018;
-        int currentYear = 0;
-        // Variables used as running totals in loop prepared
-        int repubYears = 0;
-        int demYears = 0;
-        double repubTotalPercents = 0;
-        double demTotalPercents = 0;
-        // Iterate through house dataset and tax datset, keep track of total # of democract years and republican years and the percentage of personal income tax for each
-        while(!(currentYear == endYear)){
-            String houseLine = houseScanner.nextLine();
-            Scanner houseLineScanner = new Scanner (houseLine);
-            String [] houseLineAA = houseLine.split(",");
-            //updating currentYear so it can be checked in the condition of the while loop
-            currentYear = Integer.parseInt(houseLineAA[houseYearIndex]);
-            double demPercentSeatsWon = Double.parseDouble(houseLineAA[demPercentSeatsWonIndex]);
-            // Go into tax datset and get the taxation percentage from the correct year
-            double percentIncomeTaxOfNextYears = 0;
-            for (int i = 0; i < 2; i++){
-                String taxLine = taxScanner.nextLine();
-                Scanner taxLineScanner = new Scanner(taxLine);
-                String [] taxLineAA = taxLine.split(",");
-                percentIncomeTaxOfNextYears = percentIncomeTaxOfNextYears + Double.parseDouble(taxLineAA[incomeTaxIndex]);
-                taxLineScanner.close();
-            }
-            
-            // now according to whether or not it's a democrat majority year, the general variables are updated
-            if ((double)demPercentSeatsWon > 50.0){
-                demYears++;
-                demTotalPercents = demTotalPercents + percentIncomeTaxOfNextYears;
-            }
-            else{
-                repubYears++;
-                repubTotalPercents = repubTotalPercents + percentIncomeTaxOfNextYears;
-            }
-        }
-        double averagePercentFromIncomeTaxRepublican = repubTotalPercents/repubYears;
-        double averagePercentFromIncomeTaxDemocrat = demTotalPercents/demYears;
-        System.out.println("Republican Average Percent from Individual Income Taxes:" + averagePercentFromIncomeTaxRepublican + "/n" + "Democrat Average Percent From Individual Income Taxes: " + averagePercentFromIncomeTaxDemocrat);
+    /* This is a helper method that will determine the years within the house file that are "democrat years " and the ones that are "republican years" and 
+    will return an arrayList holding an arrayList of these years separately. 
+
+    Note: this method returns an arraylist of arraylists of Strings rather than of Integers because these years will be compared to the years ine tax dataset. 
+    Within the tax dataset, some of the years are estimates for example, the year 2018 is listed in the year column as "2018 estimate." Therefore, by 
+    saving the years as ints, the .equals method can be used. In the case of this data, we do not want to utilize estimated values. So, 2018 would not be equal 
+    to "2018 estimate," preserving the years as strings allows that distinction to be evaluated using the .equals method */
+    public static ArrayList <ArrayList<String>> splitIntoRepubAndDemYears(File houseStatistics, int minYear, int maxYear) throws fileNotFoundException{
+        // Instantiate two ArrayLists, one to hold years with a democratic majority, one to hold years with a republican majority
+        // Use the header so that the "Year" column and the "Percentage of seats wonc" can be indexed
+        // Saved these indexes as yearIndex and percentageDemSeatsWonIndex
+        // Open a Scanner to iterate through the "House Dataset" file
+        // Scan each line individually, and save each line as an array of strings using split method
+        // For each line, get the year and save to currentYear variable
+        // if currentYear is in the range of >= min, <= max, do the following steps (have to use Integer.parseInt() to make this comparison)
+        // Determine whether it is a "democrat year" or "republican year" based on if the percentageDemSeatsWon is > 0 or not
+        // If it is a democratic year, add it to the democraticYears array
+        // If it is a republican year, add it to the republican years array
+        // At the end, return an ArrayList with democraticYears array as index1, republicanYears array as index 2
     }
 
     // Test to see if numbers are in the right range
